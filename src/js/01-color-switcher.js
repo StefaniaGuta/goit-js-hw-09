33a263bdc8a08e0f5c4e3ff9f55851338f24be0a
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-02-13 05:31:17 -0800

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+import throttle from "lodash.throttle";
 
+
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const body = document.body;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+     const changeColor = throttle(function() {
+        body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+
+      startButton.addEventListener("click", function(){
+        time = setInterval(changeColor, 1000);
+      });
+
+
+
+     stopButton.addEventListener('click', function() {
+        clearInterval(time);
+     });
